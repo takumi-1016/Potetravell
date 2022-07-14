7487eed437909652f7665acf2fa020284e639297
takumi-1016 <tn1016recruit@gmail.com>
2022-07-14 16:05:58 +0900

    ユーザー編集機能を実装しようとしたがわからなくなったのでROOMの方を実装していく

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show]
   get 'home/index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+}
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
